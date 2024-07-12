f08bfb854b816011523e42edaa61e129385d1ded
Juan Gómez <jgomezja@unal.edu.co>
2024-07-12 09:32:18 -0500

    <feat> (pipeline primary): creación de la lógica del pipeline primary; donde se da sentido a los datos de acuerdo al objetivo del proyecto. Tratamiento de: -nulos -duplicados -outliers -recategorizaciones

---
+++ src/functions/primary.py
@@ -0,0 +1,217 @@
+from typing import Dict, Any
+
+import pandas as pd
+import unicodedata
+import logging
+
+logging.basicConfig()
+logger = logging.getLogger(__name__)
+logger.setLevel(logging.INFO)
+
+class pipelie_primary:
+
+    # 1. Recategorizar columnas
+    def recategorize_pd(df: pd.DataFrame, params: Dict[str, Any]) -> pd.DataFrame:
+        """
+        Recategoriza los valores de las columnas especificadas en un DataFrame basado en
+        las categorías proporcionadas en los parámetros.
+
+        Parameters
+        ----------
+        df : pd.DataFrame
+            DataFrame de entrada del cual se recategorizarán los valores.
+        params : Dict[str, Any]
+            Parámetros primary.
+
+        Returns
+        -------
+        pd.DataFrame: DataFrame con los valores recategorizados.
+        """
+        logger.info("Iniciando el proceso de recategorización...")
+
+        # Parámetros
+        campos_recategorizacion = params['campos_recategorizacion']
+
+        for campo, recategorizaciones in campos_recategorizacion.items():
+            if campo in df.columns:
+                logger.info(f"Recategorizando el campo '{campo}'")
+                for recategorizacion in recategorizaciones:
+                    for original, nuevo in recategorizacion.items():
+                        df[campo] = df[campo].replace(original, nuevo)
+                        logger.info(f"Recategorizado valor '{original}' a '{nuevo}' en el campo '{campo}'")
+            else:
+                logger.warning(f"El campo '{campo}' no se encuentra en el DataFrame y será omitido.")
+
+        logger.info("Recategorización completada!")
+        return df
+
+    #2. Impurtar datos faltantes
+    def impute_missing_values_pd(df: pd.DataFrame, params: Dict[str, Any]) -> pd.DataFrame:
+        """
+        Imputa los valores faltantes en un DataFrame de Pandas basado en el tipo de cada columna
+        y excluye el campo de ID especificado en los parámetros.
+
+        Parameters
+        ----------
+        df : pd.DataFrame
+            DataFrame de Pandas sobre el cual se realizarán las imputaciones.
+        params : Dict[str, Any]
+            Parámetros primary.
+
+        Returns
+        -------
+        pd.DataFrame
+            DataFrame con los valores faltantes imputados.
+        """
+        logger.info("Iniciando el proceso de imputación de valores faltantes...")
+
+        # Obtener el nombre del campo de ID desde los parámetros
+        id_field = params['id_cliente']
+
+        # Identificar columnas con valores nulos, excluyendo el campo de ID
+        columns_with_nulls = [
+            col for col in df.columns
+            if col != id_field and (
+                    df[col].isnull().any() or
+                    (df[col] == '').any() or
+                    (df[col].astype(str).str.lower() == 'null').any() or
+                    (df[col].astype(str).str.lower() == 'none').any()
+            )
+        ]
+
+        logger.info(f"Columnas con valores nulos identificadas: {columns_with_nulls}")
+
+        # Procesar cada columna con valores nulos
+        for col in columns_with_nulls:
+            col_type = df[col].dtype
+
+            if pd.api.types.is_string_dtype(col_type):
+                # Imputar con la moda para columnas de tipo string
+                mode_value = df[col].mode().iloc[0]  # Obtener la moda
+                df[col] = df[col].replace(['', 'NULL', 'Null', 'None', None], mode_value)
+                df[col] = df[col].fillna(mode_value)
+                logger.info(f"Imputados valores nulos en la columna '{col}' con la moda: {mode_value}")
+
+            elif pd.api.types.is_integer_dtype(col_type):
+                # Imputar con la mediana para columnas de tipo int64
+                median_value = df[col].median()
+                median_value = round(median_value)  # Redondear al entero más cercano si es necesario
+                df[col] = df[col].replace(['', 'NULL', 'Null', 'None', None], median_value)
+                df[col] = df[col].fillna(median_value)
+                logger.info(f"Imputados valores nulos en la columna '{col}' con la mediana redondeada: {median_value}")
+
+            elif pd.api.types.is_float_dtype(col_type):
+                # Imputar con la mediana para columnas de tipo float64
+                median_value = df[col].median()
+                df[col] = df[col].replace(['', 'NULL', 'Null', 'None', None], median_value)
+                df[col] = df[col].fillna(median_value)
+                logger.info(f"Imputados valores nulos en la columna '{col}' con la mediana: {median_value}")
+
+        # Verificar y manejar nulos en el campo de ID
+        if id_field and df[id_field].isnull().any().any():
+            logger.warning(f"Se encontraron valores nulos en el campo de ID '{id_field}'.")
+
+            # Recomendación técnica: eliminar registros con ID nulo
+            df = df.dropna(subset=[id_field])
+            logger.info(f"Registros con valores nulos en el campo de ID '{id_field}' han sido eliminados.")
+
+        logger.info("Imputación de valores faltantes completada!")
+        return df
+
+    # 3. Eliminar clientes con id duplicado
+    def remove_duplicates_pd(df: pd.DataFrame, params: Dict[str, Any]) -> pd.DataFrame:
+        """
+        Elimina duplicados en el DataFrame basado en el campo de ID especificado en los parámetros,
+        quedándose con el último registro para cada ID duplicado.
+
+        Parameters
+        ----------
+        df : pd.DataFrame
+            DataFrame de entrada del cual se removerán los duplicados.
+        params : Dict[str, Any]
+            Parámetros primary.
+
+        Returns
+        -------
+        pd.DataFrame: DataFrame sin duplicados en el campo de ID.
+        """
+        logger.info("Iniciando el proceso de eliminación de duplicados...")
+
+        # Obtener el nombre del campo de ID desde los parámetros
+        id_field = params['id_cliente']
+
+        # Asegurarse de que id_field es una cadena
+        if isinstance(id_field, list):
+            if len(id_field) == 1:
+                id_field = id_field[0]
+            else:
+                logger.error("El parámetro 'id_cliente' contiene más de un campo.")
+                raise ValueError("El parámetro 'id_cliente' debe contener solo un campo de ID.")
+
+        if id_field not in df.columns:
+            logger.error(f"El campo de ID '{id_field}' no se encuentra en el DataFrame.")
+            raise ValueError(f"El campo de ID '{id_field}' no se encuentra en el DataFrame.")
+
+        # Verificar si hay duplicados en el campo de ID
+        has_duplicates = df.duplicated(subset=[id_field]).any()
+
+        if has_duplicates:
+            logger.info(
+                f"Se encontraron duplicados en el campo de ID '{id_field}'. Eliminando duplicados y conservando el último registro...")
+
+            # Eliminar duplicados y conservar el último registro
+            df_clean = df.drop_duplicates(subset=[id_field], keep='last')
+            logger.info(f"Duplicados eliminados. Total de registros después de la eliminación: {len(df_clean)}")
+        else:
+            logger.info(f"No se encontraron duplicados en el campo '{id_field}'.")
+            df_clean = df
+
+        logger.info("Fanaliza la verificación de duplicados!")
+        return df_clean
+
+    # 4. Remover outliers
+    def remove_outliers_pd(df: pd.DataFrame) -> pd.DataFrame:
+        """
+        Remueve outliers de un DataFrame en múltiples columnas numéricas (float64 e int64)
+        utilizando el rango intercuartílico (IQR).
+
+        Parameters
+        ----------
+        df : pd.DataFrame
+            DataFrame de entrada del cual se removerán los outliers.
+
+        Returns
+        -------
+        pd.DataFrame: DataFrame filtrado sin outliers en las columnas especificadas.
+        """
+        logger.info("Iniciando el proceso de eliminación de outliers...")
+
+        # Filtrar solo las columnas numéricas (float64 e int64)
+        numeric_columns = df.select_dtypes(include=['float64', 'int64']).columns
+        logger.info(f"Columnas numéricas identificadas para la eliminación de outliers: {numeric_columns.tolist()}")
+
+        df_clean = df.copy()  # Hacemos una copia del DataFrame original para no modificarlo directamente
+
+        for column_name in numeric_columns:
+            # Calcular el primer y tercer cuartil (Q1 y Q3)
+            Q1 = df_clean[column_name].quantile(0.25)
+            Q3 = df_clean[column_name].quantile(0.75)
+            IQR = Q3 - Q1
+
+            # Determinar los límites inferior y superior para identificar outliers
+            lower_bound = Q1 - 1.5 * IQR
+            upper_bound = Q3 + 1.5 * IQR
+
+            # Verificar si existen outliers en la columna
+            has_outliers = df_clean[
+                (df_clean[column_name] < lower_bound) | (df_clean[column_name] > upper_bound)].any().any()
+
+            if has_outliers:
+                logger.info(f"Se encontraron outliers en la columna '{column_name}'. Removiendo outliers...")
+                # Filtrar el DataFrame para remover outliers
+                df_clean = df_clean[(df_clean[column_name] >= lower_bound) & (df_clean[column_name] <= upper_bound)]
+            else:
+                logger.info(f"No se encontraron outliers en la columna '{column_name}'.")
+
+        logger.info("Eliminación de outliers completada.")
+        return df_clean
